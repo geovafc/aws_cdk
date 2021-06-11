d87bd3c9d5693fcdb9ee0258e4e255e222ee45a3
Geovane Freitas <geovane.freitasc@gmail.com>
2021-06-11 11:34:22 -0300

    Criação da minha primeira stack na AWS utilizando CDK

---
+++ src/main/java/com/myorg/VpcStack.java
@@ -0,0 +1,23 @@
+package com.myorg;
+
+import software.amazon.awscdk.core.Construct;
+import software.amazon.awscdk.core.Stack;
+import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+
+public class VpcStack extends Stack {
+    public VpcStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public VpcStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        //Criação da VPC
+        //Escopo, que é a classe que estamos e o segundo parâmetro é um identificador
+        Vpc.Builder.create(this, "Vpc01")
+                //Quantidade máxima de zonas de disponibilidade que a nossa VPC vai atuar
+                .maxAzs(3)
+                .build();
+    }
+}
